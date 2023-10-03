811ae0b5c6595bd24c672f881b40191dab62eeac
Abhik532 <abhikmondal532@gmail.com>
2023-10-03 11:37:04 +0530

    Full Complete

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var secondStick = document.querySelector(".second-stick");
 var minuteStick = document.querySelector(".minute-stick");
 var hourStick = document.querySelector(".hour-stick");
-var date = new Date();
-var s = date.getSeconds() * 6;
-var m = date.getMinutes() * 6;
-m = m+6;
-var h = date.getHours() * 30;
-setInterval(function () {
-  s = s + 6;
-  secondStick.style.transform = `translate(-50%, -50%) rotate( ${s}deg)`;
-}, 1000);
+  
+setInterval(function(){
+  var date = new Date();
+var second = date.getSeconds();
+var minute = date.getMinutes();
+var hour = date.getHours();
+var hour_rotation = hour*30 + minute/2;
+var minute_rotation = minute*6;
+var second_rotation = second*6;
+secondStick.style.transform = `translate(-50%,-50%) rotate(${second_rotation}deg)`;
+minuteStick.style.transform = `translate(-50%,-50%) rotate(${minute_rotation}deg)`;
+hourStick.style.transform = `translate(-50%,-50%) rotate(${hour_rotation}deg)`;
+},1000);
+
